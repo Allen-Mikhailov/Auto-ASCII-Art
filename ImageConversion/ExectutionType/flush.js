bac7ba6fb57bf02851e67de36dcfe7ef358efb19
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-01-15 16:28:23 -0600

    Added new rm and execution type

---
+++ ImageConversion/ExectutionType/flush.js
@@ -0,0 +1,30 @@
+exports.generatefunction = function(execute)
+{
+    const stop = .5
+    const start = .05
+    const dif = stop-start
+    const frames = 100
+
+    const c = 5
+    function f(x)
+    {
+        return -1/(c*x+1)+1
+    }
+
+    function curve(x)
+    {
+        return f(x)/f(1)
+    }
+
+    return function(filename, settings, data, width, height)
+    {
+        const files = {}
+        for (var i = 0 ; i < frames; i++)
+        {
+            console.log(start+curve(i/frames)*dif)
+            settings.colorthreshold = start+curve(i/frames)*dif
+            files[filename+i] = execute(data, width, height)
+        }
+        return files
+    }
+}
--- ImageConversion/RenderMode/block-color.js
+++ ImageConversion/RenderMode/block-color.js
@@ -13,6 +13,20 @@ exports.render = function(pixelget, settings, data, size, args)
         return (abs(c1[0]-c2[0])+abs(c1[1]-c2[1])+abs(c1[2]-c2[2]))/div <= colorthreshold
     }
 
+    function normalizegroup(grouparray, id)
+    {
+        for (var i = 0; i < grouparray.length; i++)
+        {
+            if (grouparray[i] == id)
+            {
+                return i
+            }
+        }
+    
+        grouparray[grouparray.length] = id
+        return grouparray.length-1;
+    }
+
     function groupColor(grouparray, color)
     {
         for (var i = 0; i < grouparray.length; i++)
@@ -34,6 +48,8 @@ exports.render = function(pixelget, settings, data, size, args)
     width = size[2]
     height = size[3]
 
+    const groups = []
+
     function getpixel(x, y)
     {
         const newpos = pixelget(x, y)
@@ -43,13 +59,23 @@ exports.render = function(pixelget, settings, data, size, args)
         return [data[(y*owidth+x)*4], data[(y*owidth+x)*4+1], data[(y*owidth+x)*4+2]]
     }
 
-    for (var y = height-1; y > 0; y -= 2)
+    const newdata = []
+
+    for (var y = height; y > 0; y--)
     {
-        for (var x = 0; x < width; x += 2)
+        for (var x = 0; x < width; x++)
         {
-            const groups = [getpixel(x, y)]
-            str += blocktable[""+groupColor(groups, getpixel(x+1, y))
-                +groupColor(groups, getpixel(x, y-1))+groupColor(groups, getpixel(x+1, y-1))]
+            newdata[y*owidth+x] = groupColor(groups, getpixel(x, y))
+        }
+    }
+
+    for (var y = height-1; y > 0; y--)
+    {
+        for (var x = 0; x < width-1; x++)
+        {
+            const ng = [newdata[y*owidth+x]]
+            str += blocktable[""+normalizegroup(ng, newdata[y*owidth+x+1])
+                +normalizegroup(ng, newdata[(y-1)*owidth+x])+normalizegroup(ng, newdata[(y-1)*owidth+x+1])]
         }
         str += "\n"
     }
