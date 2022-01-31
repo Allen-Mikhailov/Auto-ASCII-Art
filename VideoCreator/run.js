d1e38fa6939b390f9ba5ae1d94b08dd158bb927e
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-01-30 20:06:04 -0600

    Video Creator Updates

--- VideoCreator/run.js
+++ VideoCreator/run.js
@@ -22,7 +22,11 @@ function CreateFrame(sizex, sizey)
     {
         data[i] = 0;
     }
-    return data;
+    return {
+        pixels: data,
+        sizex: sizex,
+        sizey: sizey
+    };
 }
 
 function area(x1, y1, x2, y2, x3, y3) {
@@ -35,24 +39,31 @@ function GetTriangleBase(x1, y1, x2, y2, x3, y3) {
     const right = Math.max(x1, x2, x3);
     const left = Math.min(x1, x2, x3);
 
-    for (var x = 0; x < pixelsx; x++) {
-        for (var y = 0; y < pixelsy; y++) {
-            if (!inbounds(x, y))
-                continue
+    const sizex = right-left
+    const sizey = up-down
 
+    const frame = CreateFrame(sizex, sizey)
+
+    for (var x = 0; x < sizex; x++) {
+        for (var y = 0; y < sizey; y++) {
             let A = area(x1, y1, x2, y2, x3, y3);
             let A1 = area(x, y, x2, y2, x3, y3);
             let A2 = area(x1, y1, x, y, x3, y3);
             let A3 = area(x1, y1, x2, y2, x, y);
 
             if (Math.abs(A - (A1 + A2 + A3)) < 1)
-                colorpixel(pixeldata, y, x, 255)
+            frame.data[y*sizex+x]
         }
     }
 }
 
-function DrawCircleWithLine(size, position, angle) {
+function GetCircleWithLine(size, position, angle) {
 
+}
+
+function DrawSprite(CanvasFrame, SpriteFrame)
+{
+    
 }
 
 const FrameData = []
