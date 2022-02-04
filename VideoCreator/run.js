ea1a3e2e5fe47546822c97369ee7facfb473aeb4
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-02-03 21:52:55 -0600

    Traingle render working

--- VideoCreator/run.js
+++ VideoCreator/run.js
@@ -55,10 +55,25 @@ function GetTriangleBase(x1, y1, x2, y2, x3, y3) {
                 frame.data[y*sizex+x] = 1
         }
     }
+
+    return frame
 }
 
-function GetCircleWithLine(size, position, angle) {
+function GetCircleWithLine(radius, angle) {
+    const Frame = CreateFrame(radius*2+1, radius*2+1)
+    for (var y = 0; y < radius*2+1; y++)
+    {
+        const offset = y*Frame.sizex
+        for (var x = 0; x < radius*2+1; x++)
+        {
+            if (Math.sqrt((x-radius)**2 + (y-radius)**2) <= radius && (x-radius)*angle <= y)
+            {
+                Frame.data[offset+x] = 1 
+            }
+        }
+    }
 
+    return Frame
 }
 
 function DrawSprite(CanvasFrame, SpriteFrame, posx, posy)
@@ -69,11 +84,14 @@ function DrawSprite(CanvasFrame, SpriteFrame, posx, posy)
     const ystart = clamp(posy, 0, CanvasFrame.sizey)-posy
     const yend = clamp(posy+SpriteFrame.sizey, 0, CanvasFrame.sizey)-posy
 
+    const CData = CanvasFrame.data
+    const SData = SpriteFrame.data
+
     for (var x = xstart; x < xend; x++)
     {
         for (var y = ystart; y < yend; y++)
         {
-            CanvasFrame.data[(y+posy)*CanvasFrame.sizex+x+posx] = SpriteFrame.data[y*SpriteFrame.sizex + x]
+            CData[(y+posy)*CanvasFrame.sizex+x+posx] = SData[y*SpriteFrame.sizex + x] || CData[(y+posy)*CanvasFrame.sizex+x+posx]
         }
     }
 }
@@ -83,19 +101,24 @@ const MainFrame = CreateFrame(Fsizex, Fsizey)
 
 //Creating Mask
 const maskFrame = CreateFrame(Fsizex, Fsizey)
-const heightcap = 100/2
-const warpstrength = .05
-const wavespeed = .03
-var x = 0
+const heightcap = 400/2
+const warpstrength = 10
+const wavespeed = .0075
 for (var i = 0; i < Fsizex; i++)
 {
-    x += wavespeed + (Math.random()-.5)*2*warpstrength 
-    height = (Fsizey-1)-Math.floor(noise(i*wavespeed, 0, 0)*heightcap)
+    height = (Fsizey-1)-Math.floor(noise(i*wavespeed, 0, 0)*heightcap*2/3 + noise(i*wavespeed*1.25, 500, 0)*heightcap/3)
     for (var y = height; y < Fsizey; y++)
     {
         maskFrame.data[y*Fsizex+i] = 1
     }
 }
+
+//Meteor Sprite
+const HeadRadius = 50
+const TailLength = 100
+const TailWidth = Math.sqrt((TailLength**2)/2)
+const MeteorFrame = GetTriangleBase(0, TailWidth, HeadRadius, TailWidth+HeadRadius, HeadRadius+TailWidth, 0)
+DrawSprite(MeteorFrame, GetCircleWithLine(HeadRadius, 0), -HeadRadius, 1)
 
 for (var i = 0; i < Frames; i++) {
     for (var j = 0; j < Fsizex*Fsizey; j++)
@@ -103,9 +126,16 @@ for (var i = 0; i < Frames; i++) {
         MainFrame.data[j] = 0;
     }
 
-    DrawSprite(MainFrame, maskFrame, 0, 0)
+    DrawSprite(MainFrame, MeteorFrame, 0, i)
+    // DrawSprite(MainFrame, maskFrame, 0, 0)
 
-    FrameData[i] = Object.assign({}, MainFrame)
+    const FrameClone = {}
+    FrameClone.sizex = MainFrame.sizex
+    FrameClone.sizey = MainFrame.sizey
+    FrameClone.data = []
+    for (var k = 0; k < FrameClone.sizex*FrameClone.sizey; k++)
+        FrameClone.data[k] = MainFrame.data[k]
+    FrameData[i] = FrameClone
 }
 
 fs.writeFileSync("video.js", "videodata = "+JSON.stringify(FrameData))
