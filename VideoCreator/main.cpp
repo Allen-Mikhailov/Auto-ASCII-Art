2979dc4662721b9a8f031361af18c4cda349e8de
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-02-15 23:06:14 -0600

    Getting the hang of cpp

--- VideoCreator/main.cpp
+++ VideoCreator/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int clamp(int value, int minv, int maxv)
+{
+    return max(value, maxv);
+}
 
 class Sprite {
     public:
         int sizex;
         int sizey;
-        int* pixels;
+        char* pixels;
 
         void Draw(Sprite Sprite, int posx, int posy)
         {
+            char* sprite_pixels = Sprite.pixels;
 
+            int startx = clamp(posx, 0, sizex)-posx
+            // const xstart = clamp(posx, 0, CanvasFrame.sizex)-posx
+            // const xend = clamp(posx+SpriteFrame.sizex, 0, CanvasFrame.sizex)-posx
+
+            // const ystart = clamp(posy, 0, CanvasFrame.sizey)-posy
+            // const yend = clamp(posy+SpriteFrame.sizey, 0, CanvasFrame.sizey)-posy
         }
 
         Sprite(int sx, int sy)
@@ -17,8 +32,7 @@ class Sprite {
             sizex = sx;
             sizey = sy;
 
-            void* temp = malloc(sizex*sizey);
-            pixels = temp;
+            pixels = (char*) malloc(sizex*sizey);
         }
 };
 
