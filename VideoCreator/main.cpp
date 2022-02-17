fedcbd03c2bd44db9d9e04d180c553206d92853a
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-02-16 23:00:25 -0600

    Cpp

--- VideoCreator/main.cpp
+++ VideoCreator/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <noise.hpp>
+
+using namespace std;
 
 int clamp(int value, int minv, int maxv)
 {
-    return std::max(std::min(value, maxv), minv);
+    return max(min(value, maxv), minv);
 }
+
+
 
 class Sprite {
     public:
@@ -42,10 +46,40 @@ class Sprite {
 };
 
 
+Sprite CircleWithLine(int radius, float angle)
+{
+    Sprite _Sprite = Sprite(radius*2+1, radius*2+1);
+    for (int y = 0; y < radius*2+1; y++)
+    {
+        int offset = y*radius*2+1;
+        for (int x = 0; x < radius*2+1; x++)
+        {
+            if (sqrt((x-radius)*(x-radius) + (y-radius)*(y-radius)))
+            {
+                _Sprite.pixels[offset+x] = 1;
+            }
+        }
+    }
+}
 
 int main(){
+    int Fsizex = 500;
+    int Fsizey = 500;
+    Sprite MainFrame = Sprite(Fsizex, Fsizey);
 
+    Sprite MaskFrame = Sprite(Fsizex, Fsizey);
 
+    double heightcap = 400/2;
+    double wavespeed = .0075;
+
+    for (int i = 0; i < Fsizex; i++)
+    {
+        int height = (Fsizey-1)-floor(noise(i*wavespeed, 0, 0)*heightcap*2/3 + noise(i*wavespeed*1.25, 500, 0)*heightcap/3);
+        for (int y = height; y < Fsizey; y++)
+        {
+            MaskFrame.pixels[y*Fsizex+i] = 1;
+        }
+    }
 
     return 0;
 }
