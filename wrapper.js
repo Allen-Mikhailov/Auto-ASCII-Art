6c497b32a9276e98bc661d88fbc6b6720d485adf
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-03 23:50:44 -0600

    Wrapper updates

--- wrapper.js
+++ wrapper.js
@@ -1,5 +1,13 @@
 const { exec } = require("child_process");
 
+const args = process.argv
+const command = args[0]
+
+if (!command)
+{
+
+}
+
 // exec("ls -la", (error, stdout, stderr) => {
 //     if (error) {
 //         console.log(`error: ${error.message}`);
