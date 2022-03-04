dc8f6810e70ade401c202a62fa4142813e4d4108
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-04 10:26:31 -0600

    Added wrapper fail states

--- wrapper.js
+++ wrapper.js
@@ -1,11 +1,33 @@
-const { exec } = require("child_process");
+const ex = require("child_process").exec;
+const flags = require("./modules/flags.js")
 
 const args = process.argv
 const command = args[0]
 
-if (!command)
+function exec(com)
+{
+    ex(com, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+}
+
+if (command == "convert")
 {
 
+} else if (command == "vidcompile") {
+
+} else if (command) {
+    console.log("\'"+command+"\' is not an ascii command")
+} else {
+    console.log("No command given")
 }
 
 // exec("ls -la", (error, stdout, stderr) => {
