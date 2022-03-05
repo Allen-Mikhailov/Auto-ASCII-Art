becc90c032fb18fc1864c0e17365a9516c2b8874
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-03-05 00:06:30 -0600

    Simple wrapper completed

--- wrapper.js
+++ wrapper.js
@@ -4,6 +4,22 @@ const flags = require("./modules/flags.js")
 const args = process.argv
 const command = args[0]
 
+function logstring(index)
+{
+    var str = ""
+    for (var i = index || 0; i < args.length-1; i++)
+    {
+        str += args[i] + " "
+    }
+
+    if (args.length > 0)
+    {
+        str += args[args.length-1]
+    }
+
+    return str
+}
+
 function exec(com)
 {
     ex(com, (error, stdout, stderr) => {
@@ -21,9 +37,9 @@ function exec(com)
 
 if (command == "convert")
 {
-
+    exec("node ./ImageConversion/index.js "+logstring(2))
 } else if (command == "vidcompile") {
-
+    exec("node ./RenderVideo/videocompile.js "+logstring(2))
 } else if (command) {
     console.log("\'"+command+"\' is not an ascii command")
 } else {
