d2e19747d116c2dac6cc1686bed09d2cc62950d5
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-04-10 23:44:11 -0500

    Setting tree completed

--- ToolCreator/UniversalModules/settingtree.js
+++ ToolCreator/UniversalModules/settingtree.js
@@ -8,7 +8,22 @@ class branch
 
     addbranch(branch, leaves)
     {
+        leaves.parent = this
         this.branches[branch] = leaves;
+    }
+
+    pass(branch) {
+        return this.branches[branch]
+    }
+
+    follow(branches)
+    {
+        head = this
+        for (var i = 0; i < branches.length; i++)
+        {
+            head.pass(branches[i])
+        }
+        return head
     }
 }
 
