4c0d6d7461775b7cffa4c7256f2bdf41da2f645f
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-04-12 11:29:41 -0500

    Flag Handler class updates

--- ToolCreator/UniversalModules/flagHandler.js
+++ ToolCreator/UniversalModules/flagHandler.js
@@ -1,10 +1,11 @@
-class flag
+class flagObj
 {
-    flag(call, primaryprefix, secondaryprefix,  description) 
+    flagObj(name, call, primaryprefix, secondaryprefix,  description) 
     {
-        if (!(call && primaryprefix))
+        if (!(name && call && primaryprefix))
             throw new TypeError("Invalid args for flag creation!")
 
+        this.name = name
         this.ppf = primaryprefix
         this.spf = secondaryprefix
         this.call = call
@@ -12,10 +13,42 @@ class flag
     }
 }
 
-class command
+class commandObj
 {
-    command() {
+    commandObj(name, call, primaryname, secondaryname) {
+        if (!(name && call && primaryname))
+            throw new TypeError("Invalid args for command creation!")
+
+        this.name = name
+        this.call = call
+        this.pname = primaryname
+        this.sname = secondaryname
+
+        this.flags = []
+        this.prefixes = {}
+    }
+
+    prefixError(flag1, flag2, flagtype) {
         
+    }
+
+    addFlag(flag)
+    {
+        this.flags.push(flag)
+
+        if (this.prefixes[flag.ppf])
+            throw new TypeError("Prefix collision in command \""+this.name+"\"\n"+
+            "with prefix \""+flag.ppf+"\" with commands +\""+flag.name+"\" and \""+this.prefixes[flag.ppf].name+"\"")
+        this.prefixes[flag.ppf] = flag
+        this.prefixes[flag.spf] = flag
+    }
+}
+
+class commandLineObj
+{
+    commandLineObj()
+    {
+        commands = {}
     }
 }
 
